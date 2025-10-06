fd264cbdf23874ee6694d7e982aeb9ff9c37230f
maxfromit <bigmaxcapacity@gmail.com>
2025-10-06 23:57:29 +0300

    feat(SalesStore): implement sales store with fetch functionality and loading state

---
+++ stores/sales.ts
@@ -0,0 +1,29 @@
+import type { Sales } from '~/server/api/sales'
+import type { CalendarRange } from '~/types'
+
+export const useSalesStore = defineStore('sales', () => {
+  const sales = ref<Sales>([])
+  const isLoading = ref(false)
+
+  const fetchSales = async (dateRange?: CalendarRange) => {
+    console.log('fetchSales called with', dateRange)
+    isLoading.value = true
+    const data = await $fetch('/api/sales', {
+      query: {
+        ...(dateRange
+          ? {
+              ...(dateRange?.start
+                ? { startDate: dateRange.start.toString() }
+                : {}),
+              ...(dateRange?.end ? { endDate: dateRange.end.toString() } : {}),
+            }
+          : {}),
+      },
+    })
+    isLoading.value = false
+    sales.value = data?.sales || []
+    console.log('sales after', sales.value)
+  }
+
+  return { sales: sales, fetchSales, isLoading }
+})
