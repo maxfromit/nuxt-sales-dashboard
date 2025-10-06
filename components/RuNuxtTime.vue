9be947c50b27a72cd066b980fbddfb9bfbc82ff8
maxfromit <bigmaxcapacity@gmail.com>
2025-10-06 23:57:14 +0300

    feat(RuNuxtTime): add RuNuxtTime component for localized datetime display

---
+++ components/RuNuxtTime.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  datetime?: string
+}>()
+</script>
+
+<template>
+  <NuxtTime
+    v-if="datetime"
+    :datetime="datetime"
+    :locale="ruLocale"
+    date-style="medium"
+  />
+</template>
