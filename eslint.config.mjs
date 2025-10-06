b3f44ddd8cd63c28233820e342ecbe31663e67df
maxfromit <bigmaxcapacity@gmail.com>
2025-10-06 23:54:57 +0300

    fix(eslint): update rules for TypeScript unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,10 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
+  }
   // Your custom configs here
 )
