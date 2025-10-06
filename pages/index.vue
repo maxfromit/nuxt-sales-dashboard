4d59a10b2b8a2e7631707f06054e09301e46701a
maxfromit <bigmaxcapacity@gmail.com>
2025-10-06 23:57:19 +0300

    feat(Dashboard): restructure dashboard layout and update theme settings dropdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import DashboardPanel from '~/components/DashboardPanel.vue'
+</script>
+
 <template>
-  <UCard
+  <div class="flex flex-1 flex-col">
+    <header class="p-4 sm:px-6 flex flex-row items-center justify-between">
+      <div class="text-xl font-semibold">Dashboard</div>
+      <ThemeSettingsDropdown />
+    </header>
+
+    <USeparator />
+
+    <DashboardPanel />
+  </div>
+
+  <!-- <UCard
     :ui="{
       body: 'flex-1 flex flex-col',
       root: 'flex-1',
@@ -9,14 +24,13 @@
     <template #header>
       <div class="flex flex-row items-center justify-between">
         <div class="text-xl font-semibold">Dashboard</div>
-        <AppearenceDropdown />
+        <ThemeSettingsDropdown />
       </div>
     </template>
 
-    <div>Body</div>
-
-    <!-- <template #footer>
+    <DashboardPanel />
+     <template #footer>
       <Placeholder class="h-8" />
-    </template> -->
-  </UCard>
+    </template> 
+  </UCard> -->
 </template>
