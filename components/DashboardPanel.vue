7d57f6ab43d5971972cd77e2ce4df3fffbf3966d
maxfromit <bigmaxcapacity@gmail.com>
2025-10-07 14:37:56 +0300

    refactor(DashboardPanel): integrate stats display

--- components/DashboardPanel.vue
+++ components/DashboardPanel.vue
@@ -3,9 +3,11 @@ import { storeToRefs } from 'pinia'
 import type { Range } from '~/types'
 
 const store = useSalesStore()
-const { isLoading, sales } = storeToRefs(store)
+const { isLoading, sales, stats } = storeToRefs(store)
 
 await callOnce(() => store.fetchSales())
+
+export type Stats = typeof stats.value
 
 const fetchSales = (range: Range) => {
   store.fetchSales(range)
@@ -21,43 +23,47 @@ const fetchSales = (range: Range) => {
     </div>
     <USeparator />
 
-    <div v-if="isLoading" class="p-4 text-sm text-muted-foreground">
-      Loading sales data...
-    </div>
+    <div class="flex flex-col gap-4 sm:gap-6 flex-1 overflow-y-auto p-4 sm:p-6">
+      <DashboardStats :stats="stats" :isLoading="isLoading" />
 
-    <div
-      v-if="!isLoading && sales.length === 0"
-      class="p-4 text-center text-sm text-muted-foreground"
-    >
-      No sales data available.
-    </div>
+      <div v-if="isLoading" class="p-4 text-sm text-muted-foreground">
+        Loading sales data...
+      </div>
 
-    <div v-else class="p-4">
-      <table class="w-full table-auto border-collapse">
-        <thead>
-          <tr class="border-b">
-            <th class="text-left p-2">ID</th>
-            <th class="text-left p-2">Amount</th>
-            <th class="text-left p-2">Date</th>
-            <th class="text-left p-2">Category</th>
-          </tr>
-        </thead>
+      <div
+        v-if="!isLoading && sales.length === 0"
+        class="p-4 text-center text-sm text-muted-foreground"
+      >
+        No sales data available.
+      </div>
 
-        <tbody>
-          <tr
-            v-for="sale in sales"
-            :key="sale.id"
-            class="border-b hover:bg-accent"
-          >
-            <td class="p-2">{{ sale.id }}</td>
-            <td class="p-2">{{ sale.amount }}</td>
-            <td class="p-2">
-              {{ new Date(sale.date).toLocaleDateString() }}
-            </td>
-            <td class="p-2">{{ sale.category }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div v-else class="p-4">
+        <table class="w-full table-auto border-collapse">
+          <thead>
+            <tr class="border-b">
+              <th class="text-left p-2">ID</th>
+              <th class="text-left p-2">Amount</th>
+              <th class="text-left p-2">Date</th>
+              <th class="text-left p-2">Category</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr
+              v-for="sale in sales"
+              :key="sale.id"
+              class="border-b hover:bg-accent"
+            >
+              <td class="p-2">{{ sale.id }}</td>
+              <td class="p-2">{{ sale.amount }}</td>
+              <td class="p-2">
+                {{ new Date(sale.date).toLocaleDateString() }}
+              </td>
+              <td class="p-2">{{ sale.category }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
---
+++ components/DashboardStats.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { Stats } from '~/components/DashboardPanel.vue'
+
+const props = defineProps<{
+  stats: Stats
+  isLoading: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-px relative grid grid-cols-1 sm:grid-cols-2"
+  >
+    <div
+      v-for="(stat, index) in stats"
+      :key="index"
+      class="lg:rounded-none first:rounded-l-lg last:rounded-r-lg items-start relative flex rounded-lg bg-elevated/50 ring ring-default"
+    >
+      <div
+        class="gap-y-1.5 relative flex flex-col flex-1 gap-x-8 p-4 sm:p-6 justify-items-start"
+      >
+        <UAvatar
+          :icon="stat.icon"
+          size="xl"
+          :ui="{ icon: 'size-4 text-primary' }"
+          class="rounded-full bg-primary/10 ring ring-inset ring-primary/25"
+        />
+        <div class="font-normal text-muted text-xs uppercase text-pretty">
+          {{ stat.label }}
+        </div>
+        <div class="text-2xl font-semibold text-highlighted">
+          {{
+            !isLoading
+              ? `${stat.prefix ? `${stat.prefix} ` : ''}${stat.value}`
+              : '...'
+          }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
