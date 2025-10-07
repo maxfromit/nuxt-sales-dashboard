0e9db4583a89be090db263557cd6d82fa14835ce
maxfromit <bigmaxcapacity@gmail.com>
2025-10-07 19:40:12 +0300

    refactor(index.vue): restructure header layout to include icon alongside title

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,10 @@ import DashboardPanel from '~/components/DashboardPanel.vue'
 <template>
   <div class="flex flex-1 flex-col">
     <header class="p-4 sm:px-6 flex flex-row items-center justify-between">
-      <div class="text-xl font-semibold">Dashboard</div>
+      <div class="flex flex-row items-center gap-2">
+        <UIcon name="i-lucide-chart-no-axes-column" class="text-secondary" />
+        <div class="text-xl font-semibold">Dashboard</div>
+      </div>
       <ThemeSettingsDropdown />
     </header>
 
