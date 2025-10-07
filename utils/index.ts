db48eee29e1dd87f8806c8b3b4b7c93335fb2c72
maxfromit <bigmaxcapacity@gmail.com>
2025-10-07 11:54:19 +0300

    refactor(utils): remove unused getTodayRange function

--- utils/index.ts
+++ utils/index.ts
@@ -4,13 +4,6 @@ import l from 'lodash'
 
 export const calendarToday = today(getLocalTimeZone())
 
-export const getTodayRange = () => {
-  return {
-    start: calendarToday.copy(),
-    end: calendarToday.copy(),
-  }
-}
-
 export const ruLocale = 'ru-RU'
 
 export const sortSalesByDate = (sales: Sales) => l.sortBy(sales, 'date')
