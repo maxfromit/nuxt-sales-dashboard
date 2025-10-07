f94355decae3639eaea5d4ffba10eeed9c6f8644
maxfromit <bigmaxcapacity@gmail.com>
2025-10-07 18:32:38 +0300

    refactor(DashboardPanel): add logic for zero data, add expandable stats for mobile

--- components/DashboardPanel.vue
+++ components/DashboardPanel.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import type { Range } from '~/types'
+import l from 'lodash'
 
 const store = useSalesStore()
 const { loading, sales, stats } = storeToRefs(store)
@@ -21,49 +22,38 @@ const fetchSales = (range: Range) => {
         <DateRangePicker @apply-date-range="fetchSales" />
       </div>
     </div>
+
     <USeparator />
 
+    <div
+      v-if="!loading && l.isEmpty(sales)"
+      class="p-4 text-center font-bold text-sm text-muted-foreground"
+    >
+      В выбранные даты нет данных для отображения.
+    </div>
+
     <div class="flex flex-col gap-4 sm:gap-6 flex-1 overflow-y-auto p-4 sm:p-6">
-      <DashboardStats :stats="stats" :loading="loading" />
+      <UCollapsible class="sm:hidden">
+        <UButton
+          label="Статистика"
+          color="neutral"
+          variant="outline"
+          trailing-icon="i-lucide-chevron-down"
+          class="group mb-2"
+          :ui="{
+            trailingIcon:
+              'group-data-[state=open]:rotate-180 transition-transform duration-200',
+          }"
+          block
+        />
 
-      <div v-if="loading" class="p-4 text-sm text-muted-foreground">
-        Loading sales data...
-      </div>
+        <template #content>
+          <DashboardStats :stats="stats" :loading="loading" />
+        </template>
+      </UCollapsible>
 
-      <div
-        v-if="!loading && sales.length === 0"
-        class="p-4 text-center text-sm text-muted-foreground"
-      >
-        No sales data available.
-      </div>
-
-      <div v-else class="p-4">
-        <table class="w-full table-auto border-collapse">
-          <thead>
-            <tr class="border-b">
-              <th class="text-left p-2">ID</th>
-              <th class="text-left p-2">Amount</th>
-              <th class="text-left p-2">Date</th>
-              <th class="text-left p-2">Category</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr
-              v-for="sale in sales"
-              :key="sale.id"
-              class="border-b hover:bg-accent"
-            >
-              <td class="p-2">{{ sale.id }}</td>
-              <td class="p-2">{{ sale.amount }}</td>
-              <td class="p-2">
-                {{ new Date(sale.date).toLocaleDateString() }}
-              </td>
-              <td class="p-2">{{ sale.category }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <DashboardStats :stats="stats" :loading="loading" class="max-sm:hidden" />
+      <DashboardChart :loading="loading" :sales="sales" />
     </div>
   </div>
 </template>
